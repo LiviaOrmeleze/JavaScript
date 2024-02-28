7fd44458248545f6bf8ba9fe2499b33cfed19582
Livia Ormeleze <lormeleze@gmail.com>
2024-02-28 16:58:46 -0300

    Atividades no JavaScript

--- Atividade05-LiviaOrmeleze.js
+++ Atividade05-LiviaOrmeleze.js
@@ -1,14 +1,33 @@
-function idade(anoDeNacismento, anoAtual) {
-  return anoAtual - anoDeNacismento;
+// function idade(anoDeNacismento, anoAtual) {
+//   return anoAtual - anoDeNacismento;
+// }
+
+// console.log(`A idade da pessoa é: ${idade(2007, 2024)} anos`);
+
+// function Viagem(lts) {
+//   return lts * 12;
+// }
+
+// var litros = 50;
+// var comb = Viagem(litros);
+
+// console.log(`O carro viaja ${comb} Kms, com ${litros} litros`);
+
+// console.log("------------------------------------------------------");
+// var idade = 13;
+// var pessoa = idade >= 16 ? "Você pode votar!" : "Você não pode votar!";
+// 45
+// console.log(pessoa);
+console.log("------------------------------------------------------");
+var idade = 18;
+switch (true) {
+  case idade <= 15:
+    console.log(`Você não pode votar!`);
+    break;
+  case idade >= 16 && idade < 18:
+    console.log(`Você pode votar, mas não é obrigatório!`);
+    break;
+  default:
+    console.log(`Você pode votar!`);
+    break;
 }
-
-console.log(`A idade da pessoa é: ${idade(2007, 2024)} anos`);
-
-function Viagem(lts) {
-  return lts * 12;
-}
-
-var litros = 50;
-var comb = Viagem(litros);
-
-console.log(`O carro viaja ${comb} Kms, com ${litros} litros`);
